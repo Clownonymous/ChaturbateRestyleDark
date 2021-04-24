6a099065ce499941184bf2f404db57b9ed1e5f57
Clownonymous <72706687+Clownonymous@users.noreply.github.com>
2021-04-24 17:11:41 -0500

    Menu changes, profile changes.

--- chaturbate-restyle.js
+++ chaturbate-restyle.js
@@ -7,7 +7,7 @@
 // @include     https://*.chaturbate.com/*
 // @include     http://chaturbate.com/*
 // @include     http://*.chaturbate.com/*
-// @version     1.0
+// @version     1.1
 // @run-at document-start
 // ==/UserScript==
 
@@ -24,9 +24,12 @@ function colorfulstyle2(css) {
 colorfulstyle2(
 
 // ----------BODY/TOP MENU/OTHER----------
+  
+// GLOBAL
+'p {color:#fff;}' +
 
 // HTML & BODY
-'html {background:#000000 !important;color:#050505; font:62.5%/1  !important;height:100%;padding:0;cursor:url(images/my-cursor.png), auto;}' +
+'html {background:#000000 !important; color:#050505; font:62.5%/1  !important; height:100%; padding:0;}' +
 'html body div#header div.section{background-image: linear-gradient(to bottom, rgb(0, 0, 0), rgb(3, 3, 3), rgb(6, 6, 6), rgb(8, 9, 9), rgb(11, 12, 12), rgb(14, 15, 15), rgb(17, 17, 18), rgb(20, 20, 21), rgb(23, 23, 24), rgb(25, 26, 27), rgb(28, 29, 30), rgb(31, 32, 33)) !important;}' +
 'html > body {background-image: linear-gradient(to bottom, rgb(33, 33, 33), rgb(33, 33, 33), rgb(33, 33, 33), rgb(32, 33, 33), rgb(32, 33, 33), rgb(32, 33, 33), rgb(32, 32, 33), rgb(32, 32, 33), rgb(32, 32, 33), rgb(31, 32, 33), rgb(31, 32, 33), rgb(31, 32, 33)) !important;}' +
 '#header {background: #020202 !important; width: 100%;}' +
@@ -35,16 +38,23 @@ colorfulstyle2(
 'strong {color:#fff !important;}' +
 
 // USER ACCOUNT PANEL
-'#user_information .bottom {background-color:#000 !important;}' +
-'#user_information .user_information_header {background-color:#000 !important;}' +
-'#user_information .bottom td tr th {color:#fff !important;}' +
+'#header #user_information {background:#222 !important; border:0 !important;}' +
+'#user_information .user_information_header {background:transparent !important;}' +
+'#user_information .bottom {background:transparent !important;}' +
+'#user_information table td, tr {color:#fff !important;}' +
+'#user_information a {color:#0d7e9f !important;}' +
 
 // NAVIGATION
-'.nav-bar {background-color:#000 !important;}' +
+'.creat {background:transparent !important;}' +
+'#header {background-color:#404040 !important;}' +
+'#nav {background:transparent !important; line-height:20px !important; height:40px !important;}' +
+'.nav-bar {background:transparent !important; border:0 !important; padding:10px !important; font-size:13px; font-family: Consolas, Baskerville, Segoe UI, sans-serif;}' +
 'div.nav-bar ul#nav li a.login-link {visibility:visible !important;}' +
   
 // TAB MENU
-'.top-section {background: transparent !important; border:0px !important; box-shadow: 0px 4px 7px 0px rgba(59, 59, 59, 0.3);}' +
+'.top-section {background: transparent !important; border:0px !important; margin-top:20px !important;}' +
+'.top-section .advanced_search_button_shell .advanced_search_button {visibility:hidden !important;}' +
+'.top-section .search_svg {left:30px !important;}' +
 
 // SUB NAVIGATION
 '.sub-nav {display: inline-block; font: 0.8666em/16px,Arial,Helvetica,sans-serif; height: 1%; list-style: none outside none; margin-left:55px !important; min-width: 690px; overflow: hidden; padding: 0px; position: relative; top: 3px;}' +
@@ -57,10 +67,13 @@ colorfulstyle2(
 // ---------------------------------------
 
 // ----------ADVANCED SEARCH BOX----------
+'.advanced_search_button {float:left !important; color:#fff !important; right:0 !important;}' +
+'div.content h2 {color:#fff !important;}' +
+'.gender-tab a, .sub-nav li>a {color:#fff !important; border:0 !important;}' +
 'div.content div.advanced_search_options {background-color:#000; width:95% !important; height: 175px !important; margin:auto !important; padding: 5px 2.5px 1.5px !important; border: none !important; color: #9F9F9F !important;}' +
 'div.content div.advanced_search_options form .button {background: #E5E2E2 !important; border: 1px solid #E5E2E2 ;box-shadow: 0px 0px 5px 2px rgba(119, 119, 119, 0.3);-moz-box-shadow: 0px 0px 5px 2px rgba(119, 119, 119, 0.3);-webkit-box-shadow: 0px 0px 5px 2px rgba(119, 119, 119, 0.3);}' +
 'div.advanced_search_options table tbody tr td.search form input[type="text"] {background: none repeat scroll 0 0 #E456D5 !important; border: 1px solid #B1B1B1;height: 20px; line-height: 25px; margin-top: 4px; padding: 2px 4px; width: 150px !important; box-shadow: inset 2px 2px 2px 1px #8E0C81 !important; border-radius: 25px !important;}' +
-
+'#advanced_search_options label {color:#0d7e9f; font-size:13px; font-family: Consolas, Baskerville, Segoe UI, sans-serif;}' +
 // ---------------------------------------
   
 // ----------MAIN CONTENT DIV----------
@@ -84,7 +97,6 @@ colorfulstyle2(
 '.list .subject {margin-top:5px !important; color:#fff !important; width:100% !important; height:75px !important;}' +
 '.list .subject a {color:#c9c9c9 !important; width:100% !important;}' +
 '.list .sub-info {color:#fff !important;}' +
-//'.list .cams::before {content: "Read this: " !important;}' +
 '.list .cams {margin-top:5px !important; font-size:16px !important; left:0 !important;}' +
 // ------------------------------------
   
@@ -92,6 +104,13 @@ colorfulstyle2(
 '.paging {border:0px !important; padding:15px !important;}' +
 '.paging a {border:0px !important; padding:2px !important; font-size:15px !important;}' +
 // ----------------------------
+  
+//--------PROFILE PAGE---------
+'.gender-tab {background:transparent !important; color:#fff !important; border:0 !important;}' +
+'.genderTabs a {color:#fff !important;}' +
+'.genderTabs a:hover {color:#ccc !important;}' +
+'.nextCamBgColor {background-color:#000 !important; border:0 !important;}' +
+//-----------------------------
 
 // ----------CHAT CONTAINER----------
 //'#ChatTabContents {background-image: linear-gradient(to right top, #646464, #535353, #434343, #343434, #252525) !important;}' +
@@ -113,6 +132,7 @@ colorfulstyle2(
 'div.featured_blog_posts {display:none !important;}' +
 '.ad, .remove_ads, #botright, .banner {display: none !important;}' +
 '.content {overflow: hidden; padding: 0 !important; margin: 0 !important;}' +
+'.footer-labels {visibility:hidden;}' +
 '.footercon {background: #000; color:#fff;}' +
 '.footercon a {display: inline-block; color:#fff !important;}' +
 '.footer-holder .nav li a {color: #fff !important; font-family: Century Gothic, sans-serif !important; font-size: 11px !important;}' +
